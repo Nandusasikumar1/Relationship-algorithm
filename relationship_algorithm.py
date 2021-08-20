3a8b9fb306edf142cc6293b4abfc644379446c4c
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-08-20 22:26:15 +0530

    Update relationship_algorithm.py

--- relationship_algorithm.py
+++ relationship_algorithm.py
@@ -1,13 +1,13 @@
 def relationship_algorithm(dictionary={},iterations=10):
-  F={}
+  
   for i in dictionary:
     F[i]=set(dictionary[i])
   for x in range(iterations):
-    for k in F:
-        for j in F[k]:
-            if j in [c for c in F ]:
-               F[k]= F[k].union(F[j])
-  return F
+    for k in dictionary:
+        for j in dictionary[k]:
+            if j in [c for c in dictionary]:
+               dictionary[k]= dictionary[k].union(dictionary[j])
+  return dictionary 
 gg={1:[2,3,4],3:[8,9,1,6,'hi nandu']}
 #example
 print(relationship_algorithm(gg))
