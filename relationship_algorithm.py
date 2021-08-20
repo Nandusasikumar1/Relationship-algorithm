1777ad76c351eeec2efb9d5c139b5ab3e2bd442f
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-08-20 22:15:38 +0530

    Update relationship_algorithm.py

--- relationship_algorithm.py
+++ relationship_algorithm.py
@@ -10,5 +10,7 @@ def relationship_algorithm(dictionary={},iterations=10):
                F[k]= F[k].union(F[j])
   return F
 gg={1:[2,3,4],3:[8,9,1,6,'hi nandu']}
+#example
 print(relationship_algorithm(gg))
-## 1: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}, 3: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}}
+#output
+# 1: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}, 3: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}}
