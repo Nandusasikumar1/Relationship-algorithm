7dd3ade964c0d950ed6d29eb4241f546ec827003
Nandu  s <74356710+Nandusasikumar1@users.noreply.github.com>
2021-08-20 22:14:11 +0530

    Update relationship_algorithm.py

--- relationship_algorithm.py
+++ relationship_algorithm.py
@@ -1,22 +1,14 @@
-def relationship_algorithm(dictionary={},iterations=100):
+def relationship_algorithm(dictionary={},iterations=10):
   F={}
   for i in dictionary:
     F[i]=set(dictionary[i])
   for x in range(iterations):
     for k in F:
-      try:
+
         for j in F[k]:
-          if j!=k:
-            F[k].update(set([z for z in F[j] if z!=k]))
-          else:
-            pass
-      except:
-        pass
+            if j in [c for c in F ]:
+               F[k]= F[k].union(F[j])
   return F
-#example
-D={1:[7,4,5,2],2:[1,3,10],3:[1,2],4:[3,2],40:[100,123,154]}
-print(relationship_algorithm(D))
-#output
-#{1: {2, 3, 4, 5, 7, 10}, 2: {1, 3, 4, 5, 7, 10}, 3: {1, 2, 4, 5, 7, 10}, 4: {1, 2, 3, 5, 7, 10}, 40: {154, 123, 100}}
-
-
+gg={1:[2,3,4],3:[8,9,1,6,'hi nandu']}
+print(relationship_algorithm(gg))
+## 1: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}, 3: {1, 2, 3, 4, 6, 8, 9, 'hi nandu'}}
